2ed90586a59b382f417e926368de82b62e1fc1b2
Abubakr Nasir <abubakr.ezalden.nasir@gmail.com>
2024-02-23 02:08:43 -0800

    check for Werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,7 +5,6 @@ require => Exec['python'],
 command => '/usr/bin/pip3 install flask==2.1.0'
 }
 
-exec {'python':
-command => '/usr/bin/which python3'
+exec {'Werkzeug':
+command => '/usr/bin/pip3 install Werkzeug==2.11'
 }
-
