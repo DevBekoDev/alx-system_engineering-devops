5fdc66eafabcbb4465a134cfbea5c2a47b2f51da
Abubakr Nasir <abubakr.ezalden.nasir@gmail.com>
2024-02-23 02:00:25 -0800

    checked for python if already installed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# Installing flask from pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+#Installing flask
+
+exec {'pip3 install flask':
+require => Exec['python'],
+command => '/usr/bin/pip3 install flask==2.1.0'
 }
+
+exec {'python':
+command => '/usr/bin/which python3'
+}
+
